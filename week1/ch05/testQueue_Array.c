3f5cfcb990cd9bf5f01256cd3d2d10e7500fe321
Formyse <91356194+Formyse@users.noreply.github.com>
2021-10-04 12:16:01 +0800

    Update testQueue_Array.c

--- week1/ch05/testQueue_Array.c
+++ week1/ch05/testQueue_Array.c
@@ -1,1 +1,28 @@
+#include"Queue.h"
 
+
+int main()
+{
+	int size;
+	printf("Please input queue size: ");
+	scanf_s("%d", &size,10);
+	Queue Q = CreateQueue(size);
+	ElementType temp;
+	printf("Please input queue elements(seperate by space): ");
+	while (!IsFull(Q))
+	{
+		scanf_s("%d", &temp,10);
+		Enqueue(temp, Q);
+	}
+
+	printf("Dequeue queue in turn: ");
+	while (!IsEmpty(Q))
+	{
+		printf("%3d\n", FrontAndDequeue(Q));
+	}
+	printf("\n");
+	DisposeQueue(Q);
+	return 0;
+
+
+}
