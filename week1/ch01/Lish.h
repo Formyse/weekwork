a4a3bd6a76f48f10c456c753a3448980f7c57647
Formyse <91356194+Formyse@users.noreply.github.com>
2021-10-04 11:53:24 +0800

    Update Lish.h

--- week1/ch01/Lish.h
+++ week1/ch01/Lish.h
@@ -1,1 +1,26 @@
+#pragma once
+#ifndef List.h
+struct Node;
+typedef struct Node *ptrToNode;
+typedef ptrToNode List;
+typedef ptrToNode Position;
+
+typedef int ElementType;
+
+List CreateList();
+List MakeEmpty(List L);
+int IsEmpty(List L);
+int IsLast(Position P, List L);
+void PrintList(const List L);
+Position Find(ElementType X, List L);
+void Delete(ElementType X, List L);
+Position FindPrevious(ElementType X, List L);
+void Insert(ElementType X, List L, Position P);
+void DeleteList(List L);
+Position Header(List L);
+Position First(List L);
+Position Advance(Position P);
+ElementType Data(Position P);
+
+#endif // List.h
 
