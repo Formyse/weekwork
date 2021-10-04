6449f93c9425811078593875efcc06e0ef79bf81
Formyse <91356194+Formyse@users.noreply.github.com>
2021-10-04 12:11:14 +0800

    Update testStack_Array.c

--- week1/ch03/testStack_Array.c
+++ week1/ch03/testStack_Array.c
@@ -1,1 +1,22 @@
+#include"Stack.h"
+#include<stdio.h>
 
+int main()
+{
+	Stack S;
+	int i;
+
+	S = CreateStack(12);
+	for (i = 0; i < 10; i++)
+		Push(i, S);
+
+	while (!IsEmpty(S))
+	{
+		printf("%d\n", Top(S));
+		Pop(S);
+	}
+
+	DisposeStack(S);
+	return 0;
+
+}
