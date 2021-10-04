38ef57f5b32de4f8aa0c8462c0a756368a37c086
Formyse <91356194+Formyse@users.noreply.github.com>
2021-10-04 12:17:24 +0800

    Update testQueue_List.c

--- week1/ch06/testQueue_List.c
+++ week1/ch06/testQueue_List.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include "Queue2.h"
 
+int main()
+{
+	Queue Q;
+	int i;
+
+	Q = CreateQueue();
+
+	for (i = 0; i < 10; i++)
+		EnQueue(i, Q);
+
+	PrintQueue(Q);
+
+	while (!IsEmpty(Q))
+	{
+		printf("%d\n", Front(Q));
+		Dequeue(Q);
+	}
+
+	DisposeQueue(Q);
+	getchar();
+	return 0;
+}
