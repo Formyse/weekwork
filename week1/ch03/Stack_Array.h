63b0a1976647a2f8cc1eee35a05bf499405bb3ff
Formyse <91356194+Formyse@users.noreply.github.com>
2021-10-04 12:10:16 +0800

    Update Stack_Array.h

--- week1/ch03/Stack_Array.h
+++ week1/ch03/Stack_Array.h
@@ -1,1 +1,17 @@
+#pragma once
 
+#ifndef Stack.h
+struct StackRecord;//定义结构体
+typedef struct StackRecord * Stack;//定义指向结构体的栈指针
+
+typedef int ElementType;
+int IsEmpty(Stack S);//判断是否为空
+int IsFull(Stack S);//判断是否为满
+Stack CreateStack(int MaxElements);//创建栈
+void DisposeStack(Stack S);
+void MakeEmpty(Stack S);//置空
+void Push(ElementType X, Stack S);//进栈
+ElementType Top(Stack S);//栈顶
+void Pop(Stack S);//出栈
+ElementType TopAndPop(Stack S);
+#endif // !Stack.h
