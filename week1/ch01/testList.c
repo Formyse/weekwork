06a5f9e99c92f771d7b98b6dca865f606c4a78d8
Formyse <91356194+Formyse@users.noreply.github.com>
2021-10-04 12:05:11 +0800

    Update testList.c

--- week1/ch01/testList.c
+++ week1/ch01/testList.c
@@ -3,31 +3,31 @@
 
 int main()
 {
-	List L;
+	List L;//初始化各项数据
 	Position P;
 	int i;
 	L = MakeEmpty(NULL);
 	P = Header(L);
-	PrintList(L);
+	PrintList(L);//打印
 
 	for (i = 0; i < 10; i++)
 	{
-		Insert(i, L, P);
-		PrintList(L);
-		P = Advance(P);
+		Insert(i, L, P);//插入节点
+		PrintList(L);//打印链表
+		P = Advance(P);//移动节点
 	}
 	for (i = 0; i < 10; i += 2)
-		Delete(i, L);
+		Delete(i, L);//删除偶数节点
 
 	for (i = 0; i < 10; i++)
-		if ((i % 2 == 0) == (Find(i, L) != NULL))
+		if ((i % 2 == 0) == (Find(i, L) != NULL))//判断偶节点值是否不为0
 			printf("Find fails\n");
 
 	printf("Finished deletions\n");
 
-	PrintList(L);
+	PrintList(L);//打印链表
 
-	DeleteList(L);
+	DeleteList(L);//删除链表
 	getchar();
 	return 0;
 
