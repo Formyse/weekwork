136320a12641e0122babbaf5fa061919faca79c1
Formyse <91356194+Formyse@users.noreply.github.com>
2021-10-09 18:43:11 +0800

    Update Tree.h

--- week2/ch01/Tree.h
+++ week2/ch01/Tree.h
@@ -1,1 +1,16 @@
-
+#pragma once
+#include<stdlib.h>
+#include<stdio.h>
+#ifndef Tree.h
+typedef int ElementType;
+struct TreeNode;
+typedef struct TreeNode *Position;
+typedef struct TreeNode *SearchTree;
+SearchTree MakeEmpty(SearchTree T);
+Position Find(ElementType X, SearchTree T);
+Position FindMin(SearchTree T);
+Position FindMax(SearchTree T);
+SearchTree Insert(ElementType X, SearchTree T);
+SearchTree Delete(ElementType X, SearchTree T);
+ElementType Retrieve(Position P);
+#endif // !Tree.h
