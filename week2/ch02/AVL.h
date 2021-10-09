2e75af6b818ec1a58bce29d1f299c3453c9b7563
Formyse <91356194+Formyse@users.noreply.github.com>
2021-10-09 18:45:28 +0800

    Update AVL.h

--- week2/ch02/AVL.h
+++ week2/ch02/AVL.h
@@ -1,1 +1,21 @@
+#pragma once
+#ifndef AVL.h
+#include <stdio.h>
+#include <stdlib.h>
+typedef int ElementType;
+struct _AvlNode;
+typedef struct _AvlNode AvlNode;
+typedef AvlNode *Position;
+typedef AvlNode *AvlTree;
+
+AvlTree MakeEmpty(AvlTree T);
+Position Find(ElementType X, AvlTree T);
+Position FindMin(AvlTree T);
+Position FindMax(AvlTree T);
+AvlTree Insert(ElementType X, AvlTree T);
+AvlTree Delete(ElementType X, AvlTree T);
+ElementType Retrieve(Position P);
+void print_tree(int depth, int left, AvlTree T);
+void test();
+#endif // !AVL.h
 
