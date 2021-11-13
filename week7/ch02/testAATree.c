8553435943819381d05fdf675d0564ad6b7df304
Formyse <91356194+Formyse@users.noreply.github.com>
2021-11-13 23:43:55 +0800

    Create testAATree.c

---
+++ week7/ch02/testAATree.c
@@ -0,0 +1,26 @@
+#include"AATree.h"
+
+int main()
+{
+	AATree T = initialize();
+
+	T = insert(10, T);
+	T = insert(85, T);
+	T = insert(15, T);
+	T = insert(70, T);
+	T = insert(20, T);
+	T = insert(60, T);
+	T = insert(30, T);
+	T = insert(50, T);
+	T = insert(65, T);
+	T = insert(80, T);
+	T = insert(90, T);
+	T = insert(40, T);
+	T = insert(5, T);
+	T = insert(55, T);
+	T = insert(35, T);
+	PrintTree(T, T->level);
+
+	T = remove(70, T);
+	PrintTree(T, T->level);
+}
